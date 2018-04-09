c41869e67042e8d5569a792af12ea04f20525e32
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-04-09 16:14:39 -0400

    add links to favs

--- server.rb
+++ server.rb
@@ -13,3 +13,9 @@ get '/about_me' do
   @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :biography
 end
+
+get '/favourites' do
+
+  @fav_links = [ {url: 'http://mmajunkie.com/', title: 'MMA Junkie'}, {url: 'http://www.bbc.co.uk/' ,title:'BBC UK'}, {url:'https://www.github.com', title: 'Git Hub'}, {url:'https://japantoday.com/',title:'Japan Today'},{url: 'https://www.hawaiilife.com', title:'Hawaii Life'}]
+  erb :favourites
+end
